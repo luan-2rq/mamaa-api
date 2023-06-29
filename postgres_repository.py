414f996a69fa251458f0527dc98f49bdd2949723
Igor Augusto <igoraugusto1029@gmail.com>
2023-06-29 20:26:48 -0300

    adiciona novas funções

--- postgres_repository.py
+++ postgres_repository.py
@@ -14,8 +14,10 @@ class PostgresRepository:
             return None, None
 
     def save_stock(stock):
+        print('salvando stock no banco de dados')
         connection, cursor = PostgresRepository.get_connection()
         if connection is None:
+            print('não foi possivel conectar ao banco de dados')
             return False
         try:
             cursor.execute("INSERT INTO stock (name, price, timestamp) VALUES (%s, %s, %s)", (stock['name'], stock['price'], stock['timestamp']))
@@ -23,7 +25,34 @@ class PostgresRepository:
             cursor.close()
             connection.close()
             return True
-        except:
+        except Exception as e:
+            print(e)
             return False
+        
+    def get_stock_history():
+        connection, cursor = PostgresRepository.get_connection()
+        if connection is None:
+            return None
+        try:
+            cursor.execute("SELECT * FROM stock")
+            stock_history = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return stock_history
+        except:
+            return None
+        
+    def get_stock_history_by_name(name):
+        connection, cursor = PostgresRepository.get_connection()
+        if connection is None:
+            return None
+        try:
+            cursor.execute("SELECT * FROM stock WHERE name = %s", (name,))
+            stock_history = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return stock_history
+        except:
+            return None
 
         
